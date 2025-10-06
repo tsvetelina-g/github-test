fee9bed86328f81c7fb06af25fb044cdd6a072a3
Tsvetelina Gadzhelova <tsvetelina.gadzhelova@gmail.com>
2025-10-06 22:04:27 +0300

    init commit

---
+++ src/main/java/org/example/githubtest/GithubTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.githubtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubTestApplication.class, args);
+    }
+
+}
